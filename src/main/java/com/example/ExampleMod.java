16a86e392f21b7a3a10fd390554edfaf8e205974
srithamganneboina-svg <srithamganneboina@gmail.com>
2026-03-28 18:06:38 +0530

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,35 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    public static final String MOD_ID = "multibindpvp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("MultiBind PvP Mod Initialized!");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        // Register a second attack key (set to 'G' by default)
+        KeyBinding secondAttack = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.multi_bind.attack2", 
+            InputUtil.Type.KEYSYM, 
+            GLFW.GLFW_KEY_G, 
+            "category.multi_bind.pvp"
+        ));
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        // Every tick, if G is held, force the game to "Attack"
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player != null && secondAttack.isPressed()) {
+                client.options.attackKey.setPressed(true);
+            }
+        });
+    }
 }
